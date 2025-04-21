f15cb70d69626ef563c36de0c834d4807b937359
bao.nguyen <bao.nguyen@kyanon.digital>
2025-04-21 23:04:40 +0700

    d

--- services/api.ts
+++ services/api.ts
@@ -4,11 +4,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { supabase } from './supabase';
 import uuid from 'react-native-uuid';
 
-// const API_BASE_URL = 'https://api.review.tugo.com.vn/api/v1';
+const API_BASE_URL = 'https://api.review.tugo.com.vn/api/v1';
 // const API_BASE_URL = 'http://localhost:9090/api/v1';
 // const API_BASE_URL = 'http://192.168.2.102:9090/api/v1';
 // const API_BASE_URL = 'http://192.168.31.118:9090/api/v1';
-const API_BASE_URL = 'http://192.168.0.177:9090/api/v1';
+// const API_BASE_URL = 'http://192.168.0.177:9090/api/v1';
 // const API_BASE_URL = process.env.API_KD!;
 
 // Types for API responses
